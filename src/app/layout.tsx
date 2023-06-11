3a020373ea63f2e57b8d02fa5d7791a9b077e868
paaneko <paamayim.n3kudotayim@gmail.com>
2023-06-11 23:47:50 +0300

    finish header and footer + font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
-import { Exo } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
+import React from 'react';
+import { Header } from '@widgets/Header';
+import { Footer } from '@widgets/Footer';
 
-const exo = Exo({
-  weight: ['300', '400', '500', '600', '700'],
+const lato = Montserrat({
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
-  variable: '--font-exo',
+  variable: '--font-lato',
   display: 'swap',
 });
 
@@ -13,10 +16,23 @@ export const metadata = {
   description: 'This is the main page of Next Pizza!',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${exo.variable} font-sans`}>{children}</body>
+      {/* TODO create card Slot component */}
+      <body className={`${lato.variable} font-sans`}>
+        <header>
+          <Header rightContentSlot={<span>Card slot</span>} />
+        </header>
+        {children}
+        <footer>
+          <Footer />
+        </footer>
+      </body>
     </html>
   );
 }
---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,24 @@
+import { type ReactNode } from 'react';
+import { Logo } from '@shared/ui/Logo';
+
+type Props = {
+  rightContentSlot: ReactNode;
+};
+
+export function Header({ rightContentSlot }: Props) {
+  return (
+    <div className="bg-light-yellow">
+      <div className="container flex justify-between items-center py-4">
+        <Logo whiteText={false} />
+        <nav>
+          <ul className="flex space-x-3 text-lg font-semibold cursor-pointer">
+            <li>Pizza</li>
+            <li>Drinks</li>
+            <li>Desserts</li>
+          </ul>
+        </nav>
+        <div className="bg-hot-red">{rightContentSlot}</div>
+      </div>
+    </div>
+  );
+}
