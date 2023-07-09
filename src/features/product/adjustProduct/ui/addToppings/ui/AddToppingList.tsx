7ad7a4c1077859c406b187102891ad74033265b5
paaneko <paamayim.n3kudotayim@gmail.com>
2023-07-09 04:02:12 +0300

    add(addToppings): Placeholder when no toppings to add

--- src/features/product/adjustProduct/ui/addToppings/ui/AddToppingList.tsx
+++ src/features/product/adjustProduct/ui/addToppings/ui/AddToppingList.tsx
@@ -19,6 +19,22 @@ export function AddToppingList() {
       })
     );
   };
+
+  if (product.toppings.length === 0) {
+    return (
+      <div className="flex flex-col flex-grow items-center bg-dark-white rounded-lg justify-center">
+        <div className="text-4xl transform scale-150">😔</div>
+        <div className="font-bold text-4xl pt-2 pt-7">Regrettably</div>
+        <div className="font-semibold pt-4 px-20 text-center ">
+          But this exquisite creation has been meticulously crafted to deliver
+          the ultimate flavor experience, carefully balancing each ingredient.
+          Thus, we kindly ask that you savor its original form without any
+          additional toppings.
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="font-bold text-2xl text-center text-jet-black pt-5">
